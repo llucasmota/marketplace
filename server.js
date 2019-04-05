86206a2124da6c70a7f106a67646d063cdddc3ac
llucasmota <lucas.o.mota@gmail.com>
2019-04-05 08:10:07 -0300

    Foi inserido script do nodemon no package.json, ajustes no server e adicionado o model User

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const databaseConfig = require('./src/config/database')
 class App {
   constructor () {
     this.express = express()
-    this.isDev = process.env.NODE_ENV != 'production'
+    this.isDev = process.env.NODE_ENV !== 'production'
 
     this.database()
     this.middlewares()
