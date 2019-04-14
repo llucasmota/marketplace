cad53c1c4a77fae4b75f3418cb8655c1f247e16a
llucasmota <lucas.o.mota@gmail.com>
2019-04-14 19:51:52 -0300

    Adicionado novo model referente a Ad

---
+++ src/app/models/Ad.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const Ad = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  /**
+   * abaixo está sendo criado um relacionamento referenciando outro model(User)
+   * Nota: se fosse o caso ter vários autores, teríamos um array, seria a única diferença
+   */
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Ad', Ad)
