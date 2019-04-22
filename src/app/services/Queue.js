29749210782367f8d0c37b6223ab1d79b8d9ea1f
llucasmota <lucas.o.mota@gmail.com>
2019-04-22 09:20:24 -0300

    Adicionado o Redis e biblioteca Kue para lidar com o job de email, retirando do controller este trabalho e passando para um estrutura de job

---
+++ src/app/services/Queue.js
@@ -0,0 +1,12 @@
+const kue = require('kue')
+const redisConfig = require('../../config/redis')
+const jobs = require('../jobs')
+
+const Queue = kue.createQueue({ redis: redisConfig })
+
+/**
+ * Queue.process(processar os jobs da fila)
+ */
+Queue.process(jobs.PurchaseMail.key, jobs.PurchaseMail.handle)
+
+module.exports = Queue
