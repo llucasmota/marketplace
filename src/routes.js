c9238a8b086df7281fb41e71f8d74c3042a77cd9
llucasmota <lucas.o.mota@gmail.com>
2019-04-23 09:34:50 -0300

    foi adicionada a biblioteca express-validation para servir de apoio aos arquivos de validação do Joi, nas rotas foi adicionado o parametro para uso destas validações

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 const controllers = require('./app/controllers')
 
 const authMiddleware = require('./app/middlewares/auth')
+const validators = require('./app/validators/index')
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 // toda rota a partir da rota abaixo estará sujeita ao authMiddleware
 routes.use(authMiddleware)
 /**
@@ -14,12 +24,16 @@ routes.use(authMiddleware)
  */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.update)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
 /**
  * Purchases
  */
-routes.post('/purchases', controllers.PurchaseController.store)
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 
 module.exports = routes
